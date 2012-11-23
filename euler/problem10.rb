4b940343d5678225ccb3a0c0cdc2cb395f418ff7
mars <mars@MarsBookPro.local>
2012-11-23 13:11:50 +0900

    added 10 but slow

--- euler/problem10.rb
+++ euler/problem10.rb
@@ -1,3 +1,7 @@
+# Problem 10
+# 10以下の素数の和は2 + 3 + 5 + 7 = 17である.
+# 200万以下の全ての素数の和を計算しなさい.
+
 def prime?(n)
   return false if n == 0 || n == 1
   2.upto(n-1) do |i|
@@ -11,10 +15,10 @@ def sum_prime(n)
   (0..n).each do |i|
     tmp += i if prime?(i)
   end
-
   p tmp
 end
 
 n = 2000000
 
+# slow
 sum_prime(n)
